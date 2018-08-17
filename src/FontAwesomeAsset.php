0c986b2b33f7c6e41190e2cc6075269724bd4aa4
Huijie Wei <huijiewei@outlook.com>
2018-08-17 15:35:27 +0800

    Commit

---
+++ src/FontAwesomeAsset.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * User: Huijiewei
+ * Date: 2015-03-31
+ * Time: 15:29.
+ */
+namespace huijiewei\fontawesome;
+
+use yii\web\AssetBundle;
+
+class FontAwesomeAsset extends AssetBundle
+{
+    public $sourcePath = '@npm/font-awesome';
+
+    public $css = [
+        'css/font-awesome.min.css',
+    ];
+}
